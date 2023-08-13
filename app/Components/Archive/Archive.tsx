ad77c5be5c8d6d6d6c759be847d5b12b6a6127c0
Binod Budhathoki <100508632+Binod69@users.noreply.github.com>
2023-08-13 23:06:48 +0545

    added new components

--- app/Components/Archive/Archive.tsx
+++ app/Components/Archive/Archive.tsx
@@ -1,6 +1,9 @@
+'use client';
+import { useState } from 'react';
 import ArchiveCard from '../ArchiveCard/ArchiveCard';
-
+import { motion } from 'framer-motion';
 const Archive = () => {
+  const [showMore, setShowMore] = useState(false);
   return (
     <>
       <div className="max-w-contentContainer mx-auto px-4 py-24">
@@ -21,6 +24,127 @@ const Archive = () => {
             listItem={['React', 'Bootstrap', 'React-Router']}
             link={'https://kindergarten-eight.vercel.app/'}
           />
+          <ArchiveCard
+            title={' Kindergarden'}
+            desc={
+              ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrumculpa voluptas commodi sequi eveniet iusto tempore cumquedignissimos voluptatum quod!'
+            }
+            listItem={['React', 'Bootstrap', 'React-Router']}
+            link={'https://kindergarten-eight.vercel.app/'}
+          />
+          <ArchiveCard
+            title={' Kindergarden'}
+            desc={
+              ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrumculpa voluptas commodi sequi eveniet iusto tempore cumquedignissimos voluptatum quod!'
+            }
+            listItem={['React', 'Bootstrap', 'React-Router']}
+            link={'https://kindergarten-eight.vercel.app/'}
+          />
+          <ArchiveCard
+            title={' Kindergarden'}
+            desc={
+              ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrumculpa voluptas commodi sequi eveniet iusto tempore cumquedignissimos voluptatum quod!'
+            }
+            listItem={['React', 'Bootstrap', 'React-Router']}
+            link={'https://kindergarten-eight.vercel.app/'}
+          />
+          <ArchiveCard
+            title={' Kindergarden'}
+            desc={
+              ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrumculpa voluptas commodi sequi eveniet iusto tempore cumquedignissimos voluptatum quod!'
+            }
+            listItem={['React', 'Bootstrap', 'React-Router']}
+            link={'https://kindergarten-eight.vercel.app/'}
+          />
+          <ArchiveCard
+            title={' Kindergarden'}
+            desc={
+              ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrumculpa voluptas commodi sequi eveniet iusto tempore cumquedignissimos voluptatum quod!'
+            }
+            listItem={['React', 'Bootstrap', 'React-Router']}
+            link={'https://kindergarten-eight.vercel.app/'}
+          />
+          {showMore && (
+            <>
+              <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ delay: 0.1 }}
+              >
+                <ArchiveCard
+                  title={' Kindergarden'}
+                  desc={
+                    ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrumculpa voluptas commodi sequi eveniet iusto tempore cumquedignissimos voluptatum quod!'
+                  }
+                  listItem={['React', 'Bootstrap', 'React-Router']}
+                  link={'https://kindergarten-eight.vercel.app/'}
+                />
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ delay: 0.2 }}
+              >
+                <ArchiveCard
+                  title={' Kindergarden'}
+                  desc={
+                    ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrumculpa voluptas commodi sequi eveniet iusto tempore cumquedignissimos voluptatum quod!'
+                  }
+                  listItem={['React', 'Bootstrap', 'React-Router']}
+                  link={'https://kindergarten-eight.vercel.app/'}
+                />
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ delay: 0.3 }}
+              >
+                <ArchiveCard
+                  title={' Kindergarden'}
+                  desc={
+                    ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrumculpa voluptas commodi sequi eveniet iusto tempore cumquedignissimos voluptatum quod!'
+                  }
+                  listItem={['React', 'Bootstrap', 'React-Router']}
+                  link={'https://kindergarten-eight.vercel.app/'}
+                />
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ delay: 0.4 }}
+              >
+                <ArchiveCard
+                  title={' Kindergarden'}
+                  desc={
+                    ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrumculpa voluptas commodi sequi eveniet iusto tempore cumquedignissimos voluptatum quod!'
+                  }
+                  listItem={['React', 'Bootstrap', 'React-Router']}
+                  link={'https://kindergarten-eight.vercel.app/'}
+                />
+              </motion.div>
+            </>
+          )}
+        </div>
+        <div className="mt-12 flex items-center justify-center">
+          {showMore ? (
+            <>
+              <button
+                onClick={() => setShowMore(false)}
+                className="w-36 h-12 rounded-md text-textGreen text-[-13px] border border-textGreen hover:bg-hoverColor duration-300"
+              >
+                Show less
+              </button>
+            </>
+          ) : (
+            <>
+              <button
+                onClick={() => setShowMore(true)}
+                className="w-36 h-12 rounded-md text-textGreen text-[-13px] border border-textGreen hover:bg-hoverColor duration-300"
+              >
+                Show More
+              </button>
+            </>
+          )}
         </div>
       </div>
     </>
