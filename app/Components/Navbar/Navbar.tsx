a013bc9305c0311db60bb386f150bfc66ef91c92
Binod Budhathoki <100508632+Binod69@users.noreply.github.com>
2023-08-14 00:28:19 +0545

    added icons and links

--- app/Components/Navbar/Navbar.tsx
+++ app/Components/Navbar/Navbar.tsx
@@ -1,7 +1,13 @@
 'use client';
-import Link from 'next/link';
-import { motion } from 'framer-motion';
 import React, { useRef, useState } from 'react';
+import Link from 'next/link';
+import { easeIn, motion } from 'framer-motion';
+import {
+  AiOutlineClose,
+  AiFillGithub,
+  AiFillLinkedin,
+  AiFillFacebook,
+} from 'react-icons/ai';
 
 const Navbar = () => {
   const ref = useRef<string | any>('');
@@ -133,7 +139,152 @@ const Navbar = () => {
                   animate={{ x: 0, opacity: 1 }}
                   transition={{ duration: 0.1 }}
                   className="w-[80%] h-full overflow-x-scroll scrollbarHide bg-[#112240] flex flex-col items-center px-5 py-10 relative"
-                ></motion.div>
+                >
+                  <AiOutlineClose
+                    onClick={() => setShowMenu(false)}
+                    className="text-3xl text-textGreen cursor-pointer hover:text-red-500 absolute top-4 right-4"
+                  />
+                  <div>
+                    <ul className="flex flex-col text-base gap-7 py-5">
+                      <Link
+                        href="#home"
+                        className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+                        onClick={handleScroll}
+                      >
+                        <motion.li
+                          initial={{ x: 20, opacity: 0 }}
+                          animate={{ x: 0, opacity: 1 }}
+                          transition={{
+                            duration: 0.1,
+                            delay: 0.1,
+                            ease: 'easeIn',
+                          }}
+                        >
+                          Home
+                        </motion.li>
+                      </Link>
+                      <Link
+                        href="#home"
+                        className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+                        onClick={handleScroll}
+                      >
+                        <motion.li
+                          initial={{ x: 20, opacity: 0 }}
+                          animate={{ x: 0, opacity: 1 }}
+                          transition={{
+                            duration: 0.1,
+                            delay: 0.2,
+                            ease: 'easeIn',
+                          }}
+                        >
+                          <span>01.</span> About
+                        </motion.li>
+                      </Link>
+                      <Link
+                        href="#home"
+                        className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+                        onClick={handleScroll}
+                      >
+                        <motion.li
+                          initial={{ x: 20, opacity: 0 }}
+                          animate={{ x: 0, opacity: 1 }}
+                          transition={{
+                            duration: 0.1,
+                            delay: 0.3,
+                            ease: 'easeIn',
+                          }}
+                        >
+                          <span>02.</span> Experience
+                        </motion.li>
+                      </Link>
+                      <Link
+                        href="#home"
+                        className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+                        onClick={handleScroll}
+                      >
+                        <motion.li
+                          initial={{ x: 20, opacity: 0 }}
+                          animate={{ x: 0, opacity: 1 }}
+                          transition={{
+                            duration: 0.1,
+                            delay: 0.4,
+                            ease: 'easeIn',
+                          }}
+                        >
+                          <span>04.</span> Project
+                        </motion.li>
+                      </Link>
+                      <Link
+                        href="#home"
+                        className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+                        onClick={handleScroll}
+                      >
+                        <motion.li
+                          initial={{ x: 20, opacity: 0 }}
+                          animate={{ x: 0, opacity: 1 }}
+                          transition={{
+                            duration: 0.1,
+                            delay: 0.5,
+                            ease: 'easeIn',
+                          }}
+                        >
+                          <span>05.</span> Contact
+                        </motion.li>
+                      </Link>
+                    </ul>
+                    <Link href="/assets/myCv.pdf" target="_blank">
+                      <motion.button
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ delay: 0.6, ease: 'easeIn' }}
+                        className="w-32 h-10 rounded-md text-textGreen text-[13px] border border-textGreen hover:bg-hoverColor duration-300"
+                      >
+                        Resume
+                      </motion.button>
+                    </Link>
+                    <div className="flex justify-between items-center py-5">
+                      <Link href="https://github.com/Binod69" target="_blank">
+                        <motion.a
+                          initial={{ y: 20, opacity: 0 }}
+                          animate={{ y: 0, opacity: 1 }}
+                          transition={{ delay: 0.7, ease: 'easeIn' }}
+                        >
+                          <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2">
+                            <AiFillGithub />
+                          </span>
+                        </motion.a>
+                      </Link>
+                      <Link
+                        href="https://www.linkedin.com/in/binod-budhathoki-0b2000271/"
+                        target="_blank"
+                      >
+                        <motion.a
+                          initial={{ y: 20, opacity: 0 }}
+                          animate={{ y: 0, opacity: 1 }}
+                          transition={{ delay: 0.8, ease: 'easeIn' }}
+                        >
+                          <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2">
+                            <AiFillLinkedin />
+                          </span>
+                        </motion.a>
+                      </Link>
+                      <Link
+                        href="https://www.facebook.com/Line.between.two.dots/"
+                        target="_blank"
+                      >
+                        <motion.a
+                          initial={{ y: 20, opacity: 0 }}
+                          animate={{ y: 0, opacity: 1 }}
+                          transition={{ delay: 0.8, ease: 'easeIn' }}
+                        >
+                          <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2">
+                            <AiFillFacebook />
+                          </span>
+                        </motion.a>
+                      </Link>
+                    </div>
+                  </div>
+                </motion.div>
               </div>
             </>
           )}
