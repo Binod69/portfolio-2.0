4b1b4ffa042647877f3743bc1d82181216a76626
Binod Budhathoki <100508632+Binod69@users.noreply.github.com>
2024-02-21 11:33:25 +0545

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={montserrat.className}>
         <main>{children}</main>
+        <Analytics />
       </body>
     </html>
   );
